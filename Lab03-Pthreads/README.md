d3e31857cdd2d0f079c40958433e646357cd3e58
André Akio <andre.akio21@hotmail.com>
2023-10-25 21:06:54 -0300

    readme e codigo adicionados

---
+++ Lab03-Pthreads/README.md
@@ -0,0 +1,34 @@
+# Laboratório 3 - Pthreads
+
+## Sobre o projeto
+
+Esse projeto consiste em arrumar o código de multiplicação de matrizes utilizando Pthreads.
+Este documento contém instruções detalhadas sobre como compilar, executar e verificar os resultados do programa entregue.
+
+Testes foram realizados na plataforma AWS
+
+## Integrantes
+
+- André Akio Morita Osakawa - 32262787
+- Rafael de Souza Oliveira Cerqueira Tinôco - 32224907
+- Rodrigo Mileo Lourenço Gil - 32245920
+
+## Instalação
+
+Usando o `git`, clone o repositório usando o seguinte comando:
+
+`git clone https://github.com/byakkotig/SO-Mack.git`, depois use o comando `cd SO-Mack` para entrar na pasta e o comando `cd Lab03-Pthreads` para entrar na pasta do lab
+
+## Compilação e Execução
+
+Para compilar o código, entre na pasta do projeto e no terminal, utilize o comando `gcc mult_matriz.c -o mult_matriz` e para executá-lo digite o comando `./mult_matriz`.
+
+![alt](/Lab03-Pthreads/assets/)
+
+### Prints da execução dos códigos estão tanto na pasta `assets` quanto nas instruções de cada exercicio abaixo
+
+## Resolução do exercicio proposto
+
+Como solicitado, segue abaixo o print da execução do programa de multiplicação de matrizes arrumado utilizando pthreads.
+
+![alt](/Lab03-Pthreads/assets/)
---
+++ Lab03-Pthreads/mult_matriz.c
@@ -0,0 +1,120 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+
+int m, n, thread_count;
+double **A, *x, *y;
+double **result;
+
+void *Pth_math_vect(void *rank) {
+    long my_rank = (long) rank;
+    int i, j;
+    int local_m = m / thread_count;
+    int my_first_row = my_rank * local_m;
+    int my_last_row = (my_rank == thread_count - 1) ? m - 1 : (my_rank + 1) * local_m - 1;
+
+    if (my_first_row >= m) {
+        printf("Erro: mais threads do que linhas!\n");
+        exit(-1);
+    }
+
+    for (i = my_first_row; i <= my_last_row; i++) {
+        y[i] = 0;
+        for (j = 0; j < n; j++) {
+            y[i] += A[i][j] * x[j];
+        }
+    }
+
+    return NULL;
+}
+
+void print_matrix(double **matrix, int rows, int cols) {
+    for(int i = 0; i < rows; i++) {
+        for(int j = 0; j < cols; j++) {
+            printf("%f ", matrix[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+void print_vector(double *vector, int size) {
+    for(int i = 0; i < size; i++) {
+        printf("%f ", vector[i]);
+    }
+    printf("\n");
+}
+
+int main() {
+    printf("Digite o número de linhas (m): ");
+    scanf("%d", &m);
+
+    printf("Digite o número de colunas (n): ");
+    scanf("%d", &n);
+
+    printf("Digite o número de threads: ");
+    scanf("%d", &thread_count);
+
+    if (thread_count > m) {
+        printf("Erro: mais threads do que linhas!\n");
+        return -1;
+    }
+
+    // Alocar memória para A, x e y
+    A = malloc(m * sizeof(double *));
+    for(int i = 0; i < m; i++) {
+        A[i] = malloc(n * sizeof(double));
+    }
+    
+    x = malloc(n * sizeof(double));
+    
+    y = malloc(m * sizeof(double));
+
+    // Receber input para A e x
+    printf("Digite os elementos da matriz A:\n");
+    for(int i = 0; i < m; i++) {
+        for(int j = 0; j < n; j++) {
+            scanf("%lf", &A[i][j]);
+        }
+    }
+
+	// Imprimir a matriz A
+	printf("Matriz A:\n");
+	print_matrix(A, m, n);
+
+	printf("Digite os elementos do vetor x:\n");
+	for(int i = 0; i < n; i++) {
+		scanf("%lf", &x[i]);
+	}
+
+	// Imprimir o vetor x
+	printf("Vetor x:\n");
+	print_vector(x, n);
+
+	// Criar threads e chamar Pth_math_vect
+	pthread_t* thread_handles = malloc(thread_count * sizeof(pthread_t));
+	
+	for(long thread = 0; thread < thread_count; thread++) {
+		pthread_create(&thread_handles[thread], NULL, Pth_math_vect, (void*) thread);
+	}
+
+	for(long thread = 0; thread < thread_count; thread++) {
+		pthread_join(thread_handles[thread], NULL);
+	}
+
+	free(thread_handles);
+
+	// Armazenar os resultados de y em uma nova matriz
+	result = malloc(m * sizeof(double *));
+	for(int i = 0; i < m; i++) {
+		result[i] = malloc(sizeof(double));
+		result[i][0] = y[i];
+	}
+
+	// Imprimir a matriz resultante
+	printf("Matriz resultante:\n");
+	for(int i = 0; i < m; i++) {
+		printf("%f\n", result[i][0]);
+	}
+
+	return 0;
+}
